4351a2332b0fd149001ec405569edd6a0716be21
Marcelo Filipov <marcelo.filipov@gmail.com>
2026-03-14 19:58:51 -0300

    Encapsulamento - getters e setters

--- typescript/poo/index.ts
+++ typescript/poo/index.ts
@@ -3,16 +3,12 @@ import { Person } from "./banking/Person";
 function main() {
   const maria = new Person("Maria", "da Silva", new Date("1999-01-15"));
 
-  console.log(maria.getFullName());
+  // maria.firstName;
 
-  maria.updateFirstName("Maria Clara");
+  // maria.firstName = "Maria da Silva";
+  maria.firstName = "";
 
-  maria.getFullName();
-
-  //Property isBirthdayPassed is protected and only accessible within class  and its subclasses.
-  // maria.isBirthdayPassed();
-
-  console.log(maria.getFullName()); // Output: "Maria da Silva"
+  console.log(maria.firstName); // Output: "Maria da Silva"
 }
 
 main();
